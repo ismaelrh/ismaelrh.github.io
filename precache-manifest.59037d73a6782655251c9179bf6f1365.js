b9da1cd6bc5ed7c8a71e400e5b0c15d2f4f7c17d
Ismael Rodríguez <irodriguez@datumize.com>
2019-02-24 19:06:52 +0100

    Updated, fixed layout of contact methods

---
+++ precache-manifest.59037d73a6782655251c9179bf6f1365.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e2a8e110f84e05e00cdf",
+    "url": "/static/css/main.94db5655.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "989ac9cbba1c4d69f6ab",
+    "url": "/static/js/2.989ac9cb.chunk.js"
+  },
+  {
+    "revision": "e2a8e110f84e05e00cdf",
+    "url": "/static/js/main.e2a8e110.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "989ac9cbba1c4d69f6ab",
+    "url": "/static/css/2.17ee6732.chunk.css"
+  },
+  {
+    "revision": "09f0ed5d360216b9be290c88c36ecadd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7de48f9a007476d412f23843eb39689d.js"
+  "/precache-manifest.59037d73a6782655251c9179bf6f1365.js"
 );
 
 workbox.clientsClaim();
